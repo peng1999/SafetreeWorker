1eafee9ff2654e5379b17058c1e4d4755035b883
彭冠文 <pgw1041436954@outlook.com>
2015-11-19 14:01:06 +0800

    调整结构

--- src/SafetreeWorkerLibrary/SafetreeWorkerLibrary/Student.fs
+++ src/SafetreeWorkerLibrary/SafetreeWorkerLibrary/Student.fs
@@ -1,43 +1,6 @@
 ﻿//[<AutoOpen>]
 module SafetreeWorkerLibrary.Student
 
-open System.Net
-
-let userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0"
-
-let httpPostHtmlDocument (uri : string) parameters cookies =
-    let request = System.Net.WebRequest.CreateHttp uri
-    request.Method <- "POST"
-    request.Proxy <- null;
-    request.ServicePoint.Expect100Continue <- false
-    request.UserAgent <- userAgent
-    request.ContentType <- "application/x-www-form-urlencoded"
-    request.CookieContainer <- cookies
-
-    let postBytes =
-        parameters
-        |> Seq.map (fun (a, b : string) -> a + "=" + System.Web.HttpUtility.UrlEncode b)
-        |> String.concat "&"
-        |> System.Text.Encoding.UTF8.GetBytes
-
-    request.ContentLength <- int64 postBytes.Length
-
-    use postStream = request.GetRequestStream ()
-    postStream.Write (postBytes, 0, postBytes.Length)
-
-    use response = request.GetResponse () :?> HttpWebResponse
-    use responseStream = response.GetResponseStream ()
-    let htmlStream = responseStream
-    cookies.Add response.Cookies
-    let doc = HtmlDocument()
-    doc.Load(htmlStream)
-    doc
-
-let login (User(name, password)) cookie =
-    let uri = "http://chengdu.safetree.com.cn/"
-    //let requst = System.Net.WebRequest.CreateHttp uri
-    ()
-
 let getUnfinishedWorks user = 
     printfn "done getUnfinishedWorks"
     [Work(); Work()]
