f2ca86d2c9a4589e333113270ac003189f0c88ef
彭冠文 <pgw1041436954@outlook.com>
2015-11-27 14:59:56 +0800

    新建测试项目

---
+++ src/SafetreeWorkerLibrary/Test/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open SafetreeWorkerLibrary
+open SafetreeWorkerLibrary.Student
+open SafetreeWorkerLibrary.Web
+[<EntryPoint>]
+let main argv = 
+    
+
+    printfn "SafetreeWorker test:"
+    let name = "dyb"
+    let password = "123456"
+
+    //开始工作
+
+    let user = User(name, password)
+    login user
+    //let works = getUnfinishedWorks user
+    //printfn "%A" works
+    //works |> List.iter doWork
+
+    //停止工作
+
+    System.Console.ReadLine ()
+    0 // return an integer exit code
